0b87da19f8f94c9b16a78062fa45716641dbecd9
BJJoiner <joiner_brandon@columbusstate.edu>
2020-10-17 21:02:48 +0000

    todolist

--- index.js
+++ index.js
@@ -18,24 +18,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Connection Information for Mongo
 const Todo = require('./models/todo.model');
-const mongoDB = 'mongodb+srv://testConnection:b8RwqJYgo4hD1xhe@nodetodoexample-iqnde.mongodb.net/test?retryWrites=true&w=majority'
+const mongoDB = 'mongodb+srv://BJoiner:tFHJHogTrgQzeHhf@cluster0.pbfh4.mongodb.net/todolist?retryWrites=true&w=majority'
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
-//Couple of items
+
+//Couple of items todo
 var tasks = [];
 //completed items
 var completed = [];
 
 //get home page /
 app.get('/', function(req, res){
+    
     //query to mongoDB for todos
     Todo.find(function(err, todo){
         if(err){
             console.log(err);
         }else{
+             console.log(todo);
+            tasks = [];
+            completed = [];
+            id =[];
             for(i = 0; i< todo.length; i++){
                 if (todo[i].done){
                     completed.push(todo[i].item)                    
@@ -51,20 +57,35 @@ app.get('/', function(req, res){
 
 //add post method /addtask
 app.post('/addtask', function(req, res){
-    var newTask = req.body.newtask;
-    tasks.push(newTask);
-    //return index
-    res.redirect('/');
+    let newTodo = new Todo({
+        item: req.body.newtask,
+        done: false
+    })
+    newTodo.save(function(err, todo){
+        if (err){
+            console.log(err);
+        }else{
+            //return index
+            res.redirect('/');
+        }
+    })
+
+
 });
 
 app.post('/removetask', function(req, res){
     var removeTask = req.body.check;
     //push to completed
     if(typeof removeTask === 'string'){
-            tasks.splice(tasks.indexOf(removeTask), 1);
+        Todo.updateOne({_id: removeTask},{done:true},function(err){
+            if(err){
+                console.log(err)
+            }
+        })
+            //tasks.splice(tasks.indexOf(removeTask), 1);
     }else if(typeof removeTask === 'object'){
         for (var i = 0; i < removeTask.length; i++){
-            tasks.splice(tasks.indexOf(removeTask[i]), 1);
+            //tasks.splice(tasks.indexOf(removeTask[i]), 1);
 
         }
     }
